84d5099d57acfb450db39faba038d525d13d7271
Vitor <vitor.cid@solvian.com>
2019-06-29 14:19:30 -0300

    add some companies to subscribe list

--- sub.py
+++ sub.py
@@ -7,18 +7,25 @@ context = zmq.Context()
 # Define the socket using the "Context"
 sock = context.socket(zmq.SUB)
 
-# Define subscription and messages with prefix to accept.
-sock.setsockopt(zmq.SUBSCRIBE, "google")
-sock.setsockopt(zmq.SUBSCRIBE, "amazon")
-sock.setsockopt(zmq.SUBSCRIBE, "apple")
-sock.connect("tcp://127.0.0.1:5680")
 
-sub_count = 3
+companies_to_sub = ["google", "apple", "amazon", "ICBC",  "Bank of America", 
+			  "Royal Dutch Shell", "Wells Fargo", "ExxonMobil", "Citigroup",
+			  "Toyota Motor", "microsoft", "Alphabet",   "Volkswagen Group",
+			  "Chevron", "HSBC Holdings", "intel", "Comcast",    "Softbank",
+			  "RBC", "Nestle"]
+
+def subscribe_to_companies():
+	for i in companies_to_sub:
+		sock.setsockopt(zmq.SUBSCRIBE, i)	
+
+
+subscribe_to_companies()
+sock.connect("tcp://127.0.0.1:5680")
 message_buffer = []
 
 while True:	
     message_buffer.append(sock.recv())
-    if len(message_buffer) >= sub_count:
+    if len(message_buffer) >= len(companies_to_sub):
 		os.system('clear')
 		for i in message_buffer:
 			print i
