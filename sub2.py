d70252ea32e5daf83d4ae98eded4e10c566b7322
Vitor <vitor.cid@solvian.com>
2019-06-30 11:06:20 -0300

    add new subs

---
+++ sub2.py
@@ -0,0 +1,30 @@
+import zmq
+import os
+
+# ZeroMQ Context
+context = zmq.Context()
+
+# Define the socket using the "Context"
+sock = context.socket(zmq.SUB)
+
+
+companies_to_sub = ["Chevron", "HSBC Holdings", "intel", "Comcast","Softbank",
+	            "RBC", "Nestle"]
+
+def subscribe_to_companies():
+	for i in companies_to_sub:
+		sock.setsockopt(zmq.SUBSCRIBE, i)	
+
+
+subscribe_to_companies()
+sock.connect("tcp://127.0.0.1:5680")
+message_buffer = []
+
+while True:	
+    message_buffer.append(sock.recv())
+    if len(message_buffer) >= len(companies_to_sub):
+		os.system('clear')
+		for i in message_buffer:
+			print i
+		message_buffer = []
+
